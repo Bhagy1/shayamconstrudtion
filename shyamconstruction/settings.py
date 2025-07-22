4c7b1f0bf2adf9270f97d928f637db7bf7b8cb00
Bhagy1 <bhagyashreechoudhary886@gmail.com>
2025-07-22 06:46:47 -0700

    Renamed duplicate image files and updated HTML

--- shyamconstruction/settings.py
+++ shyamconstruction/settings.py
@@ -117,10 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for collectstatic
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shyamapp', 'static')]  # ✅ correct path
+  # for collectstatic
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
